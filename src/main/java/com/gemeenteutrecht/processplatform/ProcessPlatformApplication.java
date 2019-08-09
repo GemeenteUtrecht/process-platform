bb684b5dd5bf4aaea227906f80d166fc5f99be54
Tom Bokma <tom.bokma@ritense.com>
2019-08-09 11:17:48 +0200

    added camunda spring boot starter

--- src/main/java/com/gemeenteutrecht/processplatform/ProcessPlatformApplication.java
+++ src/main/java/com/gemeenteutrecht/processplatform/ProcessPlatformApplication.java
@@ -1,9 +1,11 @@
 package com.gemeenteutrecht.processplatform;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class ProcessPlatformApplication {
 
     public static void main(String[] args) {
