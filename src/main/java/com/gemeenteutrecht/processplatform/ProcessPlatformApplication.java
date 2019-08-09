d8ff4a7051800ac169c46ed70373f11aed27d82f
Tom Bokma <tom.bokma@ritense.com>
2019-08-09 10:58:21 +0200

    add app

---
+++ src/main/java/com/gemeenteutrecht/processplatform/ProcessPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.gemeenteutrecht.processplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessPlatformApplication.class, args);
+    }
+
+}
