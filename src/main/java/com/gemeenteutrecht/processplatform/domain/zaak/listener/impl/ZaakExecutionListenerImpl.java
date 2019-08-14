02164b5f7106a3c1ffc06b5a57c526d6f9579cef
Tom Bokma <tom.bokma@ritense.com>
2019-08-14 13:39:01 +0200

    clean code

--- src/main/java/com/gemeenteutrecht/processplatform/domain/zaak/listener/impl/ZaakExecutionListenerImpl.java
+++ src/main/java/com/gemeenteutrecht/processplatform/domain/zaak/listener/impl/ZaakExecutionListenerImpl.java
@@ -1,6 +1,5 @@
 package com.gemeenteutrecht.processplatform.domain.zaak.listener.impl;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gemeenteutrecht.processplatform.domain.catalogus.impl.StatusTypeImpl;
 import com.gemeenteutrecht.processplatform.domain.zaak.impl.ZaakImpl;
 import com.gemeenteutrecht.processplatform.domain.zaak.listener.ZaakExecutionListener;
@@ -10,28 +9,25 @@ import com.gemeenteutrecht.processplatform.service.DocumentService;
 import com.gemeenteutrecht.processplatform.service.ZaakService;
 import com.gemeenteutrecht.processplatform.service.ZaakTypeCatalogusService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
-import org.camunda.spin.impl.json.jackson.JacksonJsonNode;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class ZaakExecutionListenerImpl implements ZaakExecutionListener {
 
-    private final ObjectMapper objectMapper;
+    private final ProcessZaakHelper processZaakHelper;
     private final ZaakService zaakService;
     private final DocumentService documentService;
     private final ZaakTypeCatalogusService zaakTypeCatalogusService;
 
     public ZaakExecutionListenerImpl(
-            ObjectMapper objectMapper,
-            ZaakService zaakService,
+            ProcessZaakHelper processZaakHelper, ZaakService zaakService,
             DocumentService documentService,
             ZaakTypeCatalogusService zaakTypeCatalogusService
     ) {
-        this.objectMapper = objectMapper;
+        this.processZaakHelper = processZaakHelper;
         this.zaakService = zaakService;
         this.documentService = documentService;
         this.zaakTypeCatalogusService = zaakTypeCatalogusService;
@@ -39,16 +35,7 @@ public class ZaakExecutionListenerImpl implements ZaakExecutionListener {
 
     @Override
     public void createZaak(DelegateExecution execution) {
-
-        ZaakImpl zaak;
-        // get zaak from process vars
-        try {
-            final JacksonJsonNode variable = (JacksonJsonNode) execution.getVariable("zaak");
-            zaak = objectMapper.readValue(variable.toString(), ZaakImpl.class);
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot parse zaak");
-        }
-
+        ZaakImpl zaak = processZaakHelper.getZaakFrom(execution).orElseThrow();
         // create zaak
         zaak = zaakService.createZaak(
                 new ZaakCreateRequestImpl(
